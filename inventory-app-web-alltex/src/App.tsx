427e9c6540834696bc354f6d284bd9da9bb63540
Hyuri.dev <jeffersoonpaez@gmail.com>
2026-06-04 14:30:53 -0400

    feat: initialize frontend project with React, React Router, and Shadcn UI components

--- inventory-app-web-alltex/src/App.tsx
+++ inventory-app-web-alltex/src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router';
 import Login from './pages/login'
-import Dashboard from './pages/dashboard'
-import Inventory from './pages/inventory'
+// import Dashboard from './pages/dashboard'
+// import Inventory from './pages/inventory'
 import './App.css'
 
-function App() {
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      {/* <Route path="/inventory" element={<Inventory />} /> */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+    </>
   )
+);
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
--- inventory-app-web-alltex/src/pages/login.tsx
+++ inventory-app-web-alltex/src/pages/login.tsx
@@ -1,7 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 
 export default function Login() {
   return (
-    <div></div>
+    <div className="min-h-screen bg-[#212856] text-white flex flex-col items-center justify-center gap-6">
+      <h1 className="text-4xl font-bold">Hola desde Login</h1>
+      <Button variant="secondary" size="lg">Botón Shadcn</Button>
+    </div>
   )
 }
